aaee2b041671aa7908454bf55f9e0b8ef2854b40
Jiyavur <jiyavurrahman80@gmail.com>
2026-07-14 13:17:11 +0530

    Login page Improved

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,9 +1,26 @@
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/aqiBuddy');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB Connection Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+*/
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`Database Name: ${conn.connection.name}`);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
     process.exit(1);
